141713787c96c999d8138699d5d3749450b8bfca
lucas <mr.pine97@gmail.com>
2021-08-26 18:01:05 -0300

    primeira classe criada

---
+++ src/components/layouts/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {// atalho rcc SEMPRE CRIAR CLASSE COM LETRA MAIÚSCULA
+  render() {
+    return (
+      <div>
+        <h1>Teste</h1>
+      </div>
+    )
+  }
+}
