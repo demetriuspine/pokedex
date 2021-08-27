87637fa75e9dfbdd259201355ce70c6397297bd1
lucas <mr.pine97@gmail.com>
2021-08-26 21:54:33 -0300

    id de pokemon + todos os pokemons

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -7,17 +7,28 @@ export default class PokemonCard extends Component {
     pokemonIndex: "",
   };
 
+  componentDidMount() {
+    const {name, url} = this.props;
+    const pokemonIndex = url.split("/")[url.split("/").length - 2]
+    const imageUrl = `https://github.com/PokeApi/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
+    this.setState(
+      {
+        name,
+        imageUrl,
+        pokemonIndex, 
+      })
+  }
+
   render() {
-    const name = this.props.name;
-    const url = this.props.url;
 
     return (
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
-          <div className="card-header">
-            <h1>
-              {name}
-            </h1>
+          <h5 className="card-header">
+            {this.state.pokemonIndex}
+          </h5>
+          <div className="card-body">
+            <h6 className="card-title mx-auto">{this.state.name}</h6>
           </div>
         </div>
       </div>
