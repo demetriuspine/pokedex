a0ba047b41281e459d875d1f88e3c1ecf9d2826a
lucas <mr.pine97@gmail.com>
2021-08-29 00:46:13 -0300

    adicionando alt

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -27,9 +27,7 @@ export default class PokemonCard extends Component {
           <h5 className="card-header">
             {this.state.pokemonIndex}
           </h5>
-          <img className="pokemon-img card-image-top rounded mx-auto nt-2" src={this.state.imageUrl}>
-
-          </img>
+          <img alt={this.state.name} className="pokemon-img card-image-top rounded mx-auto nt-2" src={this.state.imageUrl}></img>
           <div className="card-body mx-auto">
             <h6 className="card-title">{this.state.name.toLowerCase()
               .split('-').map((letter) => letter.charAt(0).toUpperCase() + letter
